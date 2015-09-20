7cf2594ce017de435422d9d659dc8068ef319c77
Arıl Bozoluk <arilbozoluk@hotmail.com>
2015-09-20 14:10:07 +0300

    GEliştirmeler

--- js/res.js
+++ js/res.js
@@ -21,12 +21,13 @@ function getResponsiveStatus(){
       var time    = hours+':'+minutes;
       return time;
   };
+
   $(document).ready(function() {
      $('.selectpicker').selectpicker();
-    var availableHours = [
-      [10,00],[11,30],[13,00],[14,30],[16,00],[17,30],[19,00],[20,30],[22,00],[23,30]
-    ];
-
+     $('#rezForm').validator();
+     var availableHours = [
+       [10,00],[11,30],[13,00],[14,30],[16,00],[17,30],[19,00],[20,30],[22,00],[23,30]
+     ];
     var $dateInput = $( '#datePicker' );
     var $timeInput = $( '#timePicker' );
     var today = new Date();
@@ -78,10 +79,9 @@ function getResponsiveStatus(){
             var result = snapshot.val();
             if(result) {
               var disableArr = [];
-              for(var i=0; i<result.length; i++){
-                //timePickerConfig.disable.push(availableHours[result[i]]);
-                disableArr.push(availableHours[result[i]]);
-              }
+              snapshot.forEach(function(data) {
+                  disableArr.push(availableHours[data.val()]);
+              });
               $timeInput.pickatime('picker').set('disable',disableArr);
             }
             $("#hourLoading").remove();
@@ -95,12 +95,16 @@ function getResponsiveStatus(){
       $("#dateCss").attr("href","css/classic.date.css");
       $("#timeCss").attr("href","css/classic.time.css");
     } else {
+      timePickerConfig.container = "#timeContainer";
+      datePickerConfig.container = "#dateContainer";
       $("#pickerCss").attr("href","css/default.css");
       $("#dateCss").attr("href","css/default.date.css");
       $("#timeCss").attr("href","css/default.time.css");
     }
     $dateInput.pickadate(datePickerConfig);
     $timeInput.pickatime(timePickerConfig);
+
+
   });
   $(document).on("click", "#rezButton", function () {
      var dateVal = $("#datePicker").pickadate('picker').get('select','dd mmmm yyyy');
@@ -108,3 +112,75 @@ function getResponsiveStatus(){
      $(".modal-body #dateValue").html( dateVal );
      $(".modal-body #timeValue").html( timeVal );
 });
+var submitRez = function() {
+  $("#sendRezButton").attr("disabled",true);
+  $("#closeButton").attr("disabled",true);
+  $("#sendRezButton").append('<span id="rezLoading" class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>');
+  var availableTime = [ "10","11","13","14","16","17","19","20","22","23"];
+  var date = $("#datePicker").pickadate('picker').get('select','yyyy-mm-dd');
+  var time = $("#timePicker").pickatime('picker').get('select','HH');
+  var fbData = { "email": $('#inputEmail').val(), "personCount": $('#inputPersonCount').val(), "phone": $('#inputPhone').val(),
+  "firstname": $('#inputFirstname').val(), "lastname": $('#inputLastname').val(),
+  "reservationDate": $('#dateValue').html(), "reservationTime": $("#timeValue").html()};
+  var reservationsRef = new Firebase('"https://kacis22.firebaseio.com/reservations/'+date);
+  reservationsRef.push(fbData);
+  var availableHoursRef = new Firebase("https://kacis22.firebaseio.com/availableHours/"+date);
+  for(var i = 0; i < availableTime.length; i++) {
+    if(time === availableTime[i]) {
+      availableHoursRef.push(i);
+      break;
+    }
+  }
+  $.ajax({
+    url: 'http://kacis22.azurewebsites.net/api/mail/check',
+    dataType: 'json',
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify( { "email": $('#inputEmail').val(), "personCount": $('#inputPersonCount').val(), "phone": $('#inputPhone').val(),
+    "firstname": $('#inputFirstname').val(), "lastname": $('#inputLastname').val(),
+    "reservationDate": $('#dateValue').html(), "reservationTime": $("#timeValue").html()} ),
+    processData: false,
+    success: function( res ){
+      if(res=== "OK") {
+        alert('Rezervasyon başarılı bir şekilde yapılmıştır.');
+        location.reload();
+      } else {
+        alert('Rezervasyon esnasında sorun yaşanmıştır. Lütfen tekrar deneyiniz.');
+        location.reload();
+      }
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown){
+      alert('Rezervasyon esnasında sorun yaşanmıştır. Lütfen tekrar deneyiniz.');
+      location.reload();
+    }
+  });
+};
+$('#rezModal').on('shown.bs.modal', function () {
+  $('#inputLastname').focus();
+  $('#inputFirstname').focus();
+  //$('#rezForm').validator('validate');
+});
+var timeFunc = function() {
+  var minTime = [10,00];
+  if(today.getHours() > 0 && today.getHours() < 10){
+
+  } else if(today.getHours() > 10 && today.getHours() < 12) {
+    minTime = availableHours[1];
+  } else if(today.getHours() > 12 && today.getHours() < 13) {
+    minTime = availableHours[2];
+  } else if(today.getHours() > 13 && today.getHours() < 15) {
+    minTime = availableHours[3];
+  } else if(today.getHours() > 15 && today.getHours() < 16) {
+    minTime = availableHours[4];
+  } else if(today.getHours() > 16 && today.getHours() < 18) {
+    minTime = availableHours[5];
+  } else if(today.getHours() > 18 && today.getHours() < 19) {
+    minTime = availableHours[6];
+  } else if(today.getHours() > 19 && today.getHours() < 21) {
+    minTime = availableHours[7];
+  } else if(today.getHours() > 21 && today.getHours() < 22) {
+    minTime = availableHours[8];
+  } else if (today.getHours() > 22){
+    minTime = availableHours[9];
+  }
+}
